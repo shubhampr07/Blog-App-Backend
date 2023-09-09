c0c995b3fdedec52ff9ed51d17ea9e0b5d153ea9
Shubham Kumar Mandal <114088281+shubhampr07@users.noreply.github.com>
2023-09-09 17:59:27 +0530

    fixed the mongo uri exposed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
-mongoose.connect('mongodb+srv://admin:wVH1et8nQo1Bl9Gc@cluster0.dcsdx5g.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://admin:<yourMONGO-URI>@cluster0.dcsdx5g.mongodb.net/?retryWrites=true&w=majority')
 .then(()=>app.listen(5000))
 .then(() => console.log("success connected to monngodb"))
 .catch((err) => console.log(err));
